7d376f5d2587a1053d6be7240f2b182bd5b3e19b
farahrlicir <farahlicir15@gmail.com>
2024-03-14 12:56:21 +0100

    delete src/App.test.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        delicious:['Delicious Handrawn ,cursive'],
+        signature:['Great Vibes']
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
